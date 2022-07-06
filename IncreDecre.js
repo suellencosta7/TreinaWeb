b2b39a34cdf2bf36fe8572b0d566ce8f9c5aed4e
Suellen.Dev <87045491+suellencosta7@users.noreply.github.com>
2022-07-06 12:15:15 -0300

    Create IncreDecre.js

---
+++ IncreDecre.js
@@ -0,0 +1,18 @@
+function Incremento (inicio, fim){
+
+    for (inicio='';fim>=inicio;inicio++)
+    console.log(`${inicio}`)
+
+}
+
+console(Incremento(1,3))
+ 
+//FAZER UMA CHAMADA POR VEZ
+
+function Decremento (inicio,fim){
+    for(inicio;inicio >= fim; inicio--)
+    console.log(`${inicio}`)
+
+}
+
+console.log(Decremento(5,2))
