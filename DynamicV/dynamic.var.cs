b607041cb67798a9c96077a17977ac552a46a315
Suellen.Dev <87045491+suellencosta7@users.noreply.github.com>
2022-07-24 13:31:21 -0300

    dynamic x var
    Quando declaramos usando o var, é obrigatório declarar o valor para que de forma implícita o compilador indentifique o seu tipo primitivo. Durante a compilação 
    não é possível atribuir um  valor de outo tipo primitivo, exemplo.;
    var inteiro1 = 3;  
    inteiro1= "String";  => O compilador lançará um erro porque  já foi definido o tipo da variável inteiro1 como tipo inteiro.
    
    Equanto o dynamic durante a compilação recria o tipo primitivo, exemplo.;
    dynamic inteiro2= 4;
    inteiro2 = "String";
    
    Existem outras limitações entre eles.

---
+++ DynamicV/dynamic.var.cs
@@ -0,0 +1,17 @@
+// See https://aka.ms/new-console-template for more information bibliotecas
+
+
+
+var inteiro1 = 3; 
+dynamic inteiro2 = 4; 
+
+Console.WriteLine($"Inteiro1 {inteiro1} e inteiro2 {inteiro2}");
+
+Console.WriteLine("Atribuindo novos valores:");
+inteiro1 = 5;
+inteiro2 = "String";
+
+
+Console.WriteLine($"Inteiro1 {inteiro1} e inteiro2 {inteiro2}");
+
+
